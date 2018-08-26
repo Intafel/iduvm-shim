2a92533ddad98eb8785888da21e78521a3cb0be7
Sam Beller <sammbeller@gmail.com>
2018-08-26 11:59:52 -0400

    initial tools dependencies

---
+++ tools.go
@@ -0,0 +1,9 @@
+// +build tools
+package main
+
+// See https://github.com/golang/go/wiki/Modules#faq
+// This recommends tracking tools in a file such as this
+import (
+	"github.com/goadesign/goa/goagen"
+	"github.com/Intafel/iduvm-api/design"
+)
