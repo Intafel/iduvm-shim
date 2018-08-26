a68ba678df00717dc127efceb3f8fa8a37851119
Sam Beller <sammbeller@gmail.com>
2018-08-26 15:26:11 -0400

    correctly export OpenBrowser

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
 import (
-	"github.com/Intafel/iduvm-api"
+	"C" // Required for export to be recognized
+
+	"context"
+	"fmt"
+
+	"github.com/Intafel/iduvm-shim/client"
 )
+
+//export OpenBrowser
+func OpenBrowser(guestID string) error {
+	c := client.New(nil)
+	path := client.OpenBrowserGuestPath(guestID)
+	resp, err := c.OpenBrowserGuest(context.TODO(), path)
+	fmt.Println(resp)
+	fmt.Println(err)
+	return err
+}
+
+func main() {}
