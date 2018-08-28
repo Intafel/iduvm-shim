32a5e02c90bf742b1f85118ad7f23a659cf10c07
Sam Beller <sammbeller@gmail.com>
2018-08-28 16:46:18 -0400

    initial implementations of all API methods

--- main.go
+++ main.go
@@ -19,4 +19,44 @@ func OpenBrowser(guestID string) error {
 	return err
 }
 
+//export OpenFile
+func OpenFile(guestID string, filePath string) error {
+	c := client.New(nil)
+	path := client.OpenFileGuestPath(guestID, filePath)
+	resp, err := c.OpenFileGuest(context.TODO(), path)
+	fmt.Println(resp)
+	fmt.Println(err)
+	return err
+}
+
+//export OpenURL
+func OpenURL(guestID string, url_ string) error {
+	c := client.New(nil)
+	path := client.OpenURLGuestPath(guestID, url_)
+	resp, err := c.OpenURLGuest(context.TODO(), path)
+	fmt.Println(resp)
+	fmt.Println(err)
+	return err
+}
+
+//export StartGuest
+func StartGuest(guestID string) error {
+	c := client.New(nil)
+	path := client.StartGuestPath(guestID)
+	resp, err := c.StartGuest(context.TODO(), path)
+	fmt.Println(resp)
+	fmt.Println(err)
+	return err
+}
+
+//export StopGuest
+func StopGuest(guestID string) error {
+	c := client.New(nil)
+	path := client.StopGuestPath(guestID)
+	resp, err := c.StopGuest(context.TODO(), path)
+	fmt.Println(resp)
+	fmt.Println(err)
+	return err
+}
+
 func main() {}
